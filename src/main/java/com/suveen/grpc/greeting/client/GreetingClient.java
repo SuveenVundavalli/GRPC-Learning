4af502ef927ea6c9b3d46a9843174b6310b9f7df
Suveen Vundavalli <suveenkumar.vundavalli@gmail.com>
2019-02-02 13:50:27 +0100

    Added boilerplate greet service client

--- src/main/java/com/suveen/grpc/greeting/client/GreetingClient.java
+++ src/main/java/com/suveen/grpc/greeting/client/GreetingClient.java
@@ -1,6 +1,9 @@
 package com.suveen.grpc.greeting.client;
 
-import com.suveen.grpc.proto.dummy.DummyServiceGrpc;
+import com.suveen.grpc.proto.greet.GreetRequest;
+import com.suveen.grpc.proto.greet.GreetResponse;
+import com.suveen.grpc.proto.greet.GreetServiceGrpc;
+import com.suveen.grpc.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -17,12 +20,20 @@ public class GreetingClient {
         System.out.println("Creating stub");
 
         // DummyServiceBlockingStub is synchronized client
-        DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
+        // DummyServiceGrpc.DummyServiceBlockingStub syncClient = DummyServiceGrpc.newBlockingStub(channel);
 
         // DummyServiceFututreStub is ASync
         // DummyServiceGrpc.DummyServiceFutureStub asyncClient = DummyServiceGrpc.newFutureStub(channel);
 
         // Do something
+        GreetServiceGrpc.GreetServiceBlockingStub greetService= GreetServiceGrpc.newBlockingStub(channel);
+        Greeting greeting = Greeting.newBuilder().setFirstName("Suveen").setLastName("Vundavalli").build();
+
+        GreetRequest greetRequest = GreetRequest.newBuilder().setGreeting(greeting).build();
+
+        GreetResponse response = greetService.greet(greetRequest);
+
+        System.out.println(response.getResult());
 
 
         System.out.println("Shutting down client channel");
