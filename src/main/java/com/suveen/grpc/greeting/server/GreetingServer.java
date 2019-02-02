be81e40ee780209129b4e645ccc1f827bdb9a4b3
Suveen Vundavalli <suveenkumar.vundavalli@gmail.com>
2019-02-02 12:53:58 +0100

    Added server boiler plate code
    Created GreetingServer.java
    Created dummy.proto

---
+++ src/main/java/com/suveen/grpc/greeting/server/GreetingServer.java
@@ -0,0 +1,29 @@
+package com.suveen.grpc.greeting.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class GreetingServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hello gRPC");
+
+        Server server = ServerBuilder.forPort(50051)
+                .build();
+
+        // Starting the server
+        server.start();
+
+        // Wait for server termination
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Received shutdown request");
+            server.shutdown();
+            System.out.println("Successfully stopped the server");
+        }));
+
+        // Terminate server
+        server.awaitTermination();
+    }
+}
